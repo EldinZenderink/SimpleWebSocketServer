6ce1fbd1c2b7685c0e1cef8da9053ab931d3a949
Eldin Zenderink <s1077709@student.windesheim.nl>
2018-06-21 12:30:21 +0200

    doc fix

--- Docs/html/search/files_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['httpserversettings_2ecs',['HttpServerSettings.cs',['../_http_server_settings_8cs.html',1,'']]]
-];
--- Docs/html/search/properties_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['baseurls',['baseUrls',['../class_simple_web_socket_server_library_1_1_simple_web_socket_server_settings.html#a75a76b265e2d7b35a6eb33bcbf38efb4',1,'SimpleWebSocketServerLibrary::SimpleWebSocketServerSettings']]],
-  ['buffersize',['bufferSize',['../class_simple_web_socket_server_library_1_1_simple_web_socket_server_settings.html#a4ef28355e14363a3884552e8b7fb9567',1,'SimpleWebSocketServerLibrary::SimpleWebSocketServerSettings']]]
-];
